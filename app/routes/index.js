942d99fd6c459878a3fad11b6ce0217775d3de14
Alex Errington <contact@alex-errington.co.uk>
2016-03-14 21:48:10 +0000

    Routes, header parser

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,4 +5,15 @@ module.exports = function (app) {
         .get(function (req, res) {
             res.send("TODO: Some form of documentation");
         });
+        
+    app.route('/api/whoami')
+        .get(function(req, res){
+            var obj = 
+            {
+                ipaddress: req.headers['x-forwarded-for'],
+                language: req.headers['accept-language'].split(",")[0],
+                software: req.headers['user-agent'].match(/\(([^)]+)\)/)[1]
+            }
+            res.send(obj);
+        });
 };
